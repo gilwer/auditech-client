a497187bf8bac92542302f99fb1103fd95467fd7
gilad <giladwer@hotmail.com>
2023-05-09 19:27:13 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ViewChild } from '@angular/core';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'auditech-client';
-}
+               columnDefs: ColDef[] = [
+                       { field: 'url' },
+                       { field: 'id' },
+                       { field: 'html_url' },
+                       { field: 'diff_url' },
+                       { field: 'number' },
+                       { field: 'state' },
+                       { field: 'title' },
+                       { field: 'body' },
+                       { field: 'user' },
+                       { field: 'created_at' },
+                       { field: 'updated_at' },
+
+                   ];
+
+
+                    public defaultColDef: ColDef = {
+                      sortable: true,
+                      filter: true,
+                    };
+
+
+                    public rowData$!: Observable<any[]>;
+
+
+                    @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+                    constructor(private http: HttpClient) {}
+
+
+                    onGridReady(params: GridReadyEvent) {
+                      this.rowData$ = this.http
+                        .get<any[]>('https://fastapiauditech-1-w4937746.deta.app/pull-request');
+
+                    }
+
+
+
+             }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
+import { AgGridModule } from 'ag-grid-angular';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule
   ],
   providers: [],
   bootstrap: [AppComponent]
